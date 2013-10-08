f7493639d1dd3d02b5bf39cc93795093cd6d8170
markwiggles <markwigg@live.com>
2013-10-08 14:26:59 +1000

    initial

---
+++ digaudconsole/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DirectX.AudioVideoPlayback; // Unused at this time, assuming there will be an audio preview in the future (See NAudio below)
+using System.IO;
+using System.Xml;
+
+/*
+ This is a good site if you need information on the MusicXML file structure:
+ * http://www.musicxml.com/UserManuals/MusicXML/Content/EL-MusicXML.htm
+ * 
+ * MuseScore is a nice (free!) application that can read and write MusicXML files; I'm not sure what your ultimate goals are for this program,
+ * but if you need to create test files, or test-read a file that you created, it might be useful:
+ * http://musescore.org/
+ *
+ * Here are a couple of articles that explain some of the theory behind the TimeFrequency functions (WARNING - Horribly advanced math involved):
+ * http://en.wikipedia.org/wiki/Frequency_domain
+ * http://en.wikipedia.org/wiki/Short-time_Fourier_transform
+ *
+ * This site may be useful if you need information on note frequencies:
+ * http://inst.eecs.berkeley.edu/~ee20/sp97/demos/lec2/music.html
+ * 
+ * Here is a good explanation of how note frequencies are calculated:
+ * http://www.phys.unsw.edu.au/jw/notes.html
+ * 
+ * NAudio is a very useful (and free!) audio management library useable in C#. It may help you to simplify some of your current code (WaveFile.cs) and make it very easy to add audio playback
+ * or other advanced features later:
+ * http://naudio.codeplex.com/
+ * 
+ * Here is a detailed description of the WAV file format:
+ * http://www.sonicspot.com/guide/wavefiles.html
+ */
+
+
+namespace digaudconsole
+    {
+    class Program
+        {
+        public enum PitchConversion { C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B };
+
+        public static WaveFile m_WaveIn;
+        public static TimeFrequency timeFrequency;
+        public static float[] m_PixelArray;
+        public static MusicNote[] m_SheetMusic;
+        public static double m_BeatsPerMinute = 70;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main( string[] args )
+            {
+            Start();
+            }
+
+
+        /// <summary>
+        /// Hmmm... starts the program...
+        /// </summary>
+        static void Start()
+            {
+            string filename = "C:\\stuff\\jupiter.wav";
+            string xmlfile = "C:\\stuff\\jupiter.xml";
+
+            // Load the WAV file and create and populate a WaveFile object. Assign that object to m_WaveIn.
+            LoadWave( filename );
+
+            // Create a "Spectrum Analysis" type map of the WAV file and set m_PixelArray with the resulting data. (Currently unused)
+            FrequencyDomain();
+
+            // Read and parse a MusicXML file.
+            m_SheetMusic = ReadXML( xmlfile );
+            }
+
+
+        /// <summary>
+        /// Reads and parses a MusicXML file. The note data from the file is converted to data suitable for printing a score.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns>An array of MusicNote objects. Each object represents one note in the MusicXML file.</returns>
+        public static MusicNote[] ReadXML( string filename )
+            {
+            List<string> stepList = new List<string>( 100 );
+            List<int> octaveList = new List<int>( 100 );
+            List<int> durationList = new List<int>( 100 );
+            List<int> alterList = new List<int>( 100 );
+            MusicNote[] scoreArray;
+            int noteCount = 0;
+            bool isSharp;
+
+            // Open the MusicXML file.
+            FileStream xmlFileStream = new FileStream( filename, FileMode.Open, FileAccess.Read );
+            if (xmlFileStream == null)
+                {
+                System.Console.Write( "Failed to Open File!" );
+                }
+
+            // Create an XmlTextReader from the MusicXML input file
+            XmlTextReader xmlTextReader = new XmlTextReader( filename );
+
+            // Initialize isFinished, set it to true in the 'while' loop when the file has been parsed
+            bool isFinished = false;
+            while (!isFinished)
+                {
+                isSharp = false;
+
+                // Begin parsing the file. Skip each entry until we find a 'note' element or reach the end of the file.
+                while ((!xmlTextReader.Name.Equals( "note" ) || xmlTextReader.NodeType == XmlNodeType.EndElement) && !isFinished)
+                    {
+                    xmlTextReader.Read();
+                    if (xmlTextReader.ReadState == ReadState.EndOfFile)
+                        {
+                        isFinished = true;
+                        }
+                    }
+
+                xmlTextReader.Read();
+                xmlTextReader.Read();
+                if (xmlTextReader.Name.Equals( "rest" ))
+                    {
+                    // Do nothing
+                    }
+                else if (xmlTextReader.Name.Equals( "pitch" ))
+                    {
+                    while (!xmlTextReader.Name.Equals( "step" ))
+                        {
+                        xmlTextReader.Read();
+                        }
+
+                    xmlTextReader.Read();
+                    stepList.Add( xmlTextReader.Value );
+                    while (!xmlTextReader.Name.Equals( "octave" ))
+                        {
+                        if (xmlTextReader.Name.Equals( "alter" ) && xmlTextReader.NodeType == XmlNodeType.Element)
+                            {
+                            xmlTextReader.Read();
+                            alterList.Add( int.Parse( xmlTextReader.Value ) );
+                            isSharp = true;
+                            }
+
+                        xmlTextReader.Read();
+                        }
+
+                    xmlTextReader.Read();
+
+                    if (!isSharp)
+                        {
+                        alterList.Add( 0 );
+                        }
+
+                    isSharp = false;
+                    octaveList.Add( int.Parse( xmlTextReader.Value ) );
+                    while (!xmlTextReader.Name.Equals( "duration" ))
+                        {
+                        xmlTextReader.Read();
+                        }
+
+                    xmlTextReader.Read();
+
+                    durationList.Add( int.Parse( xmlTextReader.Value ) );
+
+                    //System.Console.Out.Write("Note ~ Pitch: " + stepList[noteCount] + alterList[noteCount] + " Octave: " + octaveList[noteCount] + " Duration: " + durationList[noteCount] + "\n");
+
+                    noteCount++;
+                    }
+                }
+
+            scoreArray = new MusicNote[noteCount];
+
+            double c0 = 16.351625;  // 16.351625 "C" in Hertz
+
+            // Iterate through the data we parsed from the MusicXML file, create and populate a MusicNote object for each note in the file.
+            for (int noteNumber = 0; noteNumber < noteCount; noteNumber++)
+                {
+                // Convert the "letter name" of each note to a note number using the PitchConversion enum.
+                int step = (int)Enum.Parse( typeof( PitchConversion ), stepList[noteNumber] );
+
+                // Calculate the frequency of the note (in Hertz), correct the frequency if the alterList element for this note is != 0
+                double frequency = c0 * Math.Pow( 2, octaveList[noteNumber] ) * (Math.Pow( 2, ((double)step + (double)alterList[noteNumber]) / 12 ));
+
+                // Now create a new MusicNote object. Pass the frequency of the note and the calculated duration.
+                // The duration is re-calculated to represent the number of samples per time interval. For example:
+                // In "Jupiter.xml", the duration values are as follows,
+                // 1=1/16
+                // 2=1/8
+                // 3=1/8 Dotted
+                // 4=1/4
+                //
+                // For a sample rate of 44100 and a tempo of 70, a quarter note (duration = 4) would be converted to 37,800 which is the number of samples that equal a 1/4 note.
+                // An 1/8 note would be converted to 18,900, etc.
+                scoreArray[noteNumber] = new MusicNote( frequency, (double)durationList[noteNumber] * 60 * m_WaveIn.m_SampleRate / (4 * m_BeatsPerMinute) );
+                }
+
+            return scoreArray;
+            }
+
+
+        /// <summary>
+        /// This is used to create a "Spectrum Analysis" of the WAV, could be used to graph wave frequency over time.
+        /// The array produced (m_PixelArray) is not currently being used by the application.
+        /// </summary>
+        public static void FrequencyDomain()
+            {
+            // Create a new TimeFrequency object, passing the actual sound data (m_WaveIn.m_Wave) as an array of floats and setting Sample Window to 2048
+            // The Sample Window tells the Fourier Transform function how many samples to aggregate into one result. The window of samples is analyzed to determine
+            // the predominant frequency in that particular range of samples.
+            timeFrequency = new TimeFrequency( m_WaveIn.m_Wave, 2048 );
+
+            m_PixelArray = new float[timeFrequency.m_TimeFrequencyData[0].Length * timeFrequency.m_WindowSampleSize / 2];
+
+            for (int outerIndex = 0; outerIndex < timeFrequency.m_WindowSampleSize / 2; outerIndex++)
+                {
+                for (int innerIndex = 0; innerIndex < timeFrequency.m_TimeFrequencyData[0].Length; innerIndex++)
+                    {
+                    m_PixelArray[outerIndex * timeFrequency.m_TimeFrequencyData[0].Length + innerIndex] = timeFrequency.m_TimeFrequencyData[outerIndex][innerIndex];
+                    }
+                }
+            }
+
+
+        /// <summary>
+        /// Loads the WAV file (filename) and parses relevant information (header, etc.)  into a WaveFile object. Sets member variable m_WaveIn.
+        /// </summary>
+        /// <param name="filename"></param>
+        public static void LoadWave( string filename )
+            {
+            // Sound File
+            FileStream fileStream = new FileStream( filename, FileMode.Open, FileAccess.Read );
+            if (fileStream == null)
+                {
+                System.Console.Write( "Failed to Open File!" );
+                }
+            else
+                {
+                m_WaveIn = new WaveFile( fileStream );
+                }
+            }
+        }
+    }
---
+++ digaudconsole/WaveFile.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace digaudconsole
+    {
+    /// <summary>
+    /// This class parses a standard WAV file and populates its member variables. It assumes a specific ordering of header information (which will work), but I would recommend using a library such as NAudio.
+    /// In addition to simplifying your code, you would also have the ability to read and process multiple types of audio files if you'd like:
+    /// http://naudio.codeplex.com/
+    /// </summary>
+    public class WaveFile
+        {
+        public float[] m_Wave; // The actual sound data as a FLOAT array
+        public byte[] m_Data; // The actual sound data as a BYTE array
+        public char[] m_ChunkID = new char[4];
+        public int m_ChunkSize;
+        public char[] m_Format = new char[4];
+        public char[] m_SubChunk1ID = new char[4];
+        public int m_SubChunk1Size;
+        public char[] m_SubChunk2ID = new char[4];
+        public int m_SubChunk2Size;
+        public short m_AudioFormat;
+        public short m_NumChannels;
+        public int m_SampleRate;
+        public int m_ByteRate;
+        public short m_BlockAlign;
+        public short m_BitsPerSample;
+
+        public WaveFile( FileStream file )
+            {
+            BinaryReader binaryReader = new BinaryReader( file );
+
+            // Pull out blocks of info for this WAV file sequentially and populate the member variables.
+            m_ChunkID = binaryReader.ReadChars( 4 );
+            m_ChunkSize = binaryReader.ReadInt32();
+            m_Format = binaryReader.ReadChars( 4 );
+            m_SubChunk1ID = binaryReader.ReadChars( 4 );
+            m_SubChunk1Size = binaryReader.ReadInt32();
+            m_AudioFormat = binaryReader.ReadInt16();
+            m_NumChannels = binaryReader.ReadInt16();
+            m_SampleRate = binaryReader.ReadInt32();
+            m_ByteRate = binaryReader.ReadInt32();
+            m_BlockAlign = binaryReader.ReadInt16();
+            m_BitsPerSample = binaryReader.ReadInt16();
+            m_SubChunk2ID = binaryReader.ReadChars( 4 );
+            m_SubChunk2Size = binaryReader.ReadInt32();
+
+            int sampleCount = m_SubChunk2Size / (m_BitsPerSample / 8);
+            m_Data = new byte[sampleCount];
+            m_Wave = new float[sampleCount];
+
+            // Read the actual sound data as an array of BYTES
+            m_Data = binaryReader.ReadBytes( sampleCount );
+
+            // Convert the BYTE array to a FLOAT array
+            // Each float will be in the range of -1.0 to 1.0 so,
+            // BYTE     FLOAT
+            // ----     -----
+            // 0        -1.0
+            // 128       0.0
+            // 255       1.0    (technically, it will be .992, but close enough. 256 would be be exactly 1.0 but a byte ranges from 0-255 )
+            for (int index = 0; index < sampleCount; index++)
+                {
+                m_Wave[index] = ((float)m_Data[index] - 128) / 128;
+                }
+            }
+        }
+    }
